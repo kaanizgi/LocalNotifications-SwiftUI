bf0d52c36b09e0dd17ce0a6a4cce93d9b27c9066
Kaan İzgi <kaanizgi1531@gmail.com>
2022-03-31 13:17:49 +0300

    Initial Commit

---
+++ LocalNotifications-SwiftUI/LocalNotifications_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotifications_SwiftUIApp.swift
+//  LocalNotifications-SwiftUI
+//
+//  Created by Kaan İzgi on 31.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotifications_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
